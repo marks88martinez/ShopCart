b39ba55e118e16a44ae12d58aaff7adac0c6931f
marcos <marks88martinez@gmail.com>
2024-08-07 21:02:20 -0400

    company

--- resources/views/sitio/company.blade.php
+++ resources/views/sitio/company.blade.php
@@ -29,7 +29,16 @@
                                 </h1>
                                 <div class="para__content">
                                     <p class="para__text">
-                                        Fundada el 8 de agosto de 1990, Krishna nació con la misión de traer productos únicos y de alta calidad al mercado de artículos de bazar. Con una pasión por lo artesanal y lo espiritual, hemos crecido desde una pequeña tienda local hasta convertirnos en un referente en la venta de artículos decorativos y espirituales.
+                                        Desde hace 34 años, Krishna S.A. se ha dedicado a ofrecer a sus clientes una amplia gama de productos de alta calidad en accesorios, utensilios y decoración para el hogar. Nuestro compromiso es proporcionar no solo productos, sino también una experiencia única que combine funcionalidad y estilo. Gracias a nuestra vasta experiencia en el sector y nuestra pasión por la innovación, hemos logrado consolidarnos como líderes en el mercado.
+                                    </p>
+                                    <p class="para__text">
+                                        En Krishna S.A., creemos que la decoración de un hogar es una expresión de la personalidad y el estilo de vida de nuestros clientes. Por eso, nos esforzamos por mantenernos al día con las últimas tendencias internacionales en diseño y decoración, viajando a diferentes países para seleccionar cuidadosamente los mejores productos. Nuestro equipo de expertos está siempre en la búsqueda de nuevas ideas y soluciones creativas que puedan transformar cualquier espacio en un lugar acogedor y elegante.
+                                    </p>
+                                    <p class="para__text">
+                                        Además de nuestra dedicación a la calidad y el estilo, también nos enorgullece nuestro excelente servicio al cliente. En nuestros dos locales, la matriz en el Shopping Vendome y la filial en el Shopping Americana, ambos ubicados en Ciudad del Este, ofrecemos una atención personalizada para asegurarnos de que cada cliente encuentre exactamente lo que busca. Nuestro personal está capacitado para brindar asesoramiento experto y ayudar a nuestros clientes a tomar las mejores decisiones para sus necesidades de decoración.
+                                    </p>
+                                    <p class="para__text">
+                                        En Krishna S.A., no solo vendemos productos, sino que también nos comprometemos a crear ambientes que inspiren y mejoren la vida de nuestros clientes. A lo largo de estos años, hemos construido una sólida reputación basada en la confianza y la satisfacción del cliente, y seguimos trabajando con la misma pasión y dedicación que al principio. Esperamos seguir siendo parte de la vida de nuestros clientes durante muchos años más, ayudándolos a convertir sus casas en verdaderos hogares.
                                     </p>
                                     {{-- <p class="para__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore aperiam fugit consequuntur voluptatibus ex sint iure in, distinctio sed dolorem aspernatur veritatis repellendus dolorum voluptate, animi libero officiis eveniet accusamus recusandae. </p> --}}
                                 </div>
